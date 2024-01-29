53fb4bd65859d18a714d3a36e91f37b8d67845ec
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-01-29 10:14:35 +0530

    first commit

---
+++ client/src/components/Homepage1.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Homepage1 = () => {
+  return <div>Homepage1</div>;
+};
---
+++ client/src/components/Homepage2.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Homepage2 = () => {
+  return <div>Homepage2</div>;
+};
